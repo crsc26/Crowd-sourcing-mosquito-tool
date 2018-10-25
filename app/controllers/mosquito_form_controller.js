572306977f9624740f6c7871d17f9eae5e6d2f9a
estebanquintana6 <a01184855@itesm.mx>
2018-10-24 21:14:44 -0500

    Post request

--- app/controllers/mosquito_form_controller.js
+++ app/controllers/mosquito_form_controller.js
@@ -6,11 +6,42 @@ app.controller("Form_Controller", function($scope, $http, NgMap)
     $scope.center = [20.61839, -100.41068];
     $scope.latlng = [20.61839, -100.41068];
 
+    $scope.type = "";
+    $scope.avistamiento = "";
+    $scope.comments = "";
+
     $scope.getpos = function (event) {
         $scope.lat = event.latLng.lat();
         $scope.lng = event.latLng.lng();
         $scope.latlng = [event.latLng.lat(), event.latLng.lng()];
     };
+
+    $scope.debug = function(){
+      var jsn = {
+        "latlng" : $scope.latlng,
+        "type" : $scope.type,
+        "seen" : $scope.avistamiento,
+        "comments" : $scope.comments
+      }
+
+      var req = {
+       method: 'POST',
+       url: 'http://example.com',
+       headers: {
+         'Content-Type': undefined
+       },
+       data: jsn
+      }
+
+      $http(req).then(function(){
+          //insert code
+      },
+      function(){
+        //error code
+      });
+      
+      console.log(jsn);
+    }
 
 
     $scope.placeMarker = function(){
