7ac0d8a2f9c90bb3b5b7d738df63f86bcd7a8ad1
Crsc <crsc_26@hotmail.com>
2018-10-25 23:34:24 -0500

    Weather now working

--- app/controllers/mosquito_form_controller.js
+++ app/controllers/mosquito_form_controller.js
@@ -9,6 +9,7 @@ app.controller("Form_Controller", function($scope, $http, NgMap)
     $scope.type = "";
     $scope.avistamiento = "";
     $scope.comments = "";
+    $scope.weatherInfo = "";
 
     $scope.getpos = function (event) {
         $scope.lat = event.latLng.lat();
@@ -49,7 +50,26 @@ app.controller("Form_Controller", function($scope, $http, NgMap)
         var loc = this.getPlace().geometry.location;
         $scope.latlng = [loc.lat(), loc.lng()];
         $scope.center = [loc.lat(), loc.lng()];
-    };
+    }
+
+    $scope.getWeather = function(){
+
+      var u = 'http://api.openweathermap.org/data/2.5/weather?lat=' + $scope.latlng[0] + '&lon=' + $scope.latlng[1] + '&appid=ecb1f756686518281c429bf5b7498d70';
+
+      $http({
+        method: 'GET',
+        url: u,
+      }).then(function successCallback(response) {
+          $scope.weatherInfo = response.data;
+          $scope.tmp = $scope.weatherInfo.main.temp - 273.15;
+          console.log($scope.weatherInfo);
+        }, function errorCallback(response) {
+          console.log(response);
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+
+    }
 
 
     var x = document.getElementById("demo");
@@ -63,12 +83,14 @@ app.controller("Form_Controller", function($scope, $http, NgMap)
     }
 
     function showPosition(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
         $scope.center = [position.coords.latitude, position.coords.longitude];
         $scope.latlng = [position.coords.latitude, position.coords.longitude];
+        console.log($scope.latlng);
+        $scope.$apply();
 
     }
+
+
 
     getLocation();
 
